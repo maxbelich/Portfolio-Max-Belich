c746153ec874b3ac3bec5cd8714fb3a91f396ab7
Max Belich <244745219+maxbelich@users.noreply.github.com>
2026-07-24 13:47:25 +0200

    feat: add reusable button component with rem helper

--- src/app/pages/home/sections/hero/hero.ts
+++ src/app/pages/home/sections/hero/hero.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Button } from '../../../../shared/components/button/button';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [Button],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
