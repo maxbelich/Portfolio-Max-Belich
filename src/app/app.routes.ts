24dc4a32261f8d1ac51a25e599550e9214176a42
Max Belich <244745219+maxbelich@users.noreply.github.com>
2026-07-24 12:50:21 +0200

    feat: add page routing and wire up layout components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { LegalNotice } from './pages/legal-notice/legal-notice';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'impressum',
+    component: LegalNotice,
+  },
+  {
+    path: 'datenschutz',
+    component: PrivacyPolicy,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Hero } from './sections/hero/hero';
+import { AboutMe } from './sections/about-me/about-me';
+import { SkillSet } from './sections/skill-set/skill-set';
+import { FeaturedProjects } from './sections/featured-projects/featured-projects';
+import { Contact } from './sections/contact/contact';
+import { Testimonials } from './sections/testimonials/testimonials';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Hero, AboutMe, SkillSet, FeaturedProjects, Testimonials, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
